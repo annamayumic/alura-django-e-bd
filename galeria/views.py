52a67f5ad7be19d55182a6abe65a9f7590df4159
anna <carvalhoannamayumi@gmail.com>
2025-01-14 14:10:50 -0300

    projeto inicial sem bd

---
+++ galeria/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+
+
+def index(request):
+    dados={
+    1:{"nome":"Nebulosa de Carina", "legenda":"Webbtelescope.org / NASA / James Webb"},
+    2:{"nome":"Galaxia NGC 1079","legenda":"nasa.org / NASA / Hubble"}
+    }
+    return render(request, 'galeria/index.html',{"cards":dados})
+
+def imagem(request):
+    return render(request, 'galeria/imagem.html')
